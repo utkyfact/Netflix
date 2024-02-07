39d1c93f685fdbeb8c3bf64081daa4e1b3497712
Utku Şen <144535374+utkyfact@users.noreply.github.com>
2024-02-07 16:53:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,40 +13,22 @@ input.addEventListener("focusout",function(){
     }
 })
 
-
 $(".accordion-baslik").click(function(){
-   let icon = $(this).children(".icon")
+   let chosenIcon = $(this).children(".icon")
+   let icon = $(".accordion-baslik").children(".icon")
    let hiden = $(this).parent().children(".accordion-icerik")
 
-   if(icon.hasClass("carpi")){
+   if(chosenIcon.hasClass("carpi")){
         hiden.slideUp()
-        icon.removeClass("carpi")
+        chosenIcon.removeClass("carpi")
+        
    }else{
+    icon.removeClass("carpi")
         $("icon").removeClass("carpi")
         $(".accordion-icerik").slideUp()
         hiden.slideToggle()
-        icon.toggleClass("carpi")
+        chosenIcon.toggleClass("carpi")
    }
 })
 
-// baslik.forEach((title)=>{
-//     title.addEventListener("click",function(){
-//         let hide = this.parentElement.children[1]
-//         let icon = this.children[1]
 
-//         if(icon.classList.contains("carpi")){
-//             icon.classList.remove("carpi")
-//             hide.classList.remove("accordion-show","p-3")
-//         }else{
-//             icerik.forEach(e=>{
-//                 e.classList.remove("accordion-show","p-3")  
-//             })
-//             ikon.forEach(e=>{
-//                 e.classList.remove("carpi") 
-//             })
-//             hide.classList.toggle("accordion-show")
-//             hide.classList.toggle("p-3")
-//             icon.classList.toggle("carpi")
-//         }
-//     })
-// })
